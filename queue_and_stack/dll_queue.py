688a69e13896a23abfa7d64e8a0cdd3a65b58e29
davidanagy <davidanagy@users.noreply.github.com>
2020-03-23 12:29:32 -0700

    completed the queue class

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,29 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # ANSWER: It's a good choice because we need to quickly access the front
+        # and end of the list, but we **only** need to access those.
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # When we queue a value, we add it to the *end* of our list--
+        # i.e., its tail.
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        # When we dequeue a value, we remove it from the **start** of
+        # our list--i.e., its head.
+        if self.size == 0:
+            # Looking at the test file, we want to return None when we
+            # attempt to de-queue from an empty queue.
+            return None
+        else:
+            self.size -= 1
+            # The remove_from_head() method gives us the removed value,
+            # so we just need to return that.
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        # Just return the size
+        return self.size
