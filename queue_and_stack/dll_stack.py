d205cd026ed2d53e7d7d1d9bf51003a9ca72c9d7
davidanagy <davidanagy@users.noreply.github.com>
2020-03-23 12:32:24 -0700

    completed stack class

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,24 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # ANSWER: Because the only position we need to access is the first one
+        # (or the last one--the point is, only one position need be accessible.)
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # Push and pop from the head.
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        # Push and pop from the head.
+        if self.size == 0:
+            # Return None when attempting to pop from an empty stack
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        # Just return the size
+        return self.size
