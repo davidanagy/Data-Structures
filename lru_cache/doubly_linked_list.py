9f0ecad0cf5b59f5156162772932923c9300f428
davidanagy <davidanagy@users.noreply.github.com>
2020-03-24 13:37:55 -0700

    completed the lru_cache code and tested successfully

--- lru_cache/doubly_linked_list.py
+++ lru_cache/doubly_linked_list.py
@@ -54,25 +54,65 @@ class DoublyLinkedList:
             new_head = self.head.prev
             self.head = new_head
         else:
-            self.head = ListNode(value)
+            new_node = ListNode(value)
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
 
     def remove_from_head(self):
-        pass
+        self.delete(self.head)
 
     def add_to_tail(self, value):
-        pass
+        if self.tail:
+            self.tail.insert_after(value)
+            new_tail = self.tail.next
+            self.tail = new_tail
+        else:
+            new_node = ListNode(value)
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
 
     def remove_from_tail(self):
-        pass
+        self.delete(self.tail)
 
     def move_to_front(self, node):
-        pass
+        if self.head is node:
+            pass
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_head(value)
 
     def move_to_end(self, node):
-        pass
+        if self.tail is node:
+            pass
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_tail(value)
 
     def delete(self, node):
-        pass
+        try:
+            node.delete()
+            self.length -= 1
+            if self.head is self.tail:
+                self.head = None
+                self.tail = None
+            elif self.head is node:
+                self.head = node.next
+            elif self.tail is node:
+                self.tail = node.prev
+        except:
+            raise ValueError('Node not found')
 
     def get_max(self):
-        pass
+        if self.length == 0:
+            return None
+        max_value = self.head.value
+        current_node = self.head
+        while current_node:
+            if current_node.value > max_value:
+                max_value = current_node.value
+            current_node = current_node.next
+        return max_value
